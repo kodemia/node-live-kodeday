6ef266ccf01ba6094e6d063e7d0994c98c32ab9b
Carlos Silva <carlos@leco.mx>
2020-07-11 14:08:24 -0500

    mongoose connection

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 
-// tamaño de la letra
-/*
-  fewfewfew
-  fewfewfew
-  fewfewfew
-*/
+const mongoose = require('mongoose')
 
-console.log('¡HOLA Koders!')
- 
+const Student = mongoose.model('Students', {
+  name: {
+    type: String,
+    required: true
+  },
+  course: {
+    type: String,
+    required: true,
+    enum: [
+      'Programacion',
+      'Ingles',
+      'Cocina'
+    ]
+  },
+  age: {
+    type: Number,
+    min: 18
+  }
+})
+
+mongoose.connect('mongodb+srv://charles:kodemia123@kodemia-node-live.eet2k.mongodb.net/school?retryWrites=true&w=majority', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}, () => {
+  console.log('DB connected')
+  Student.create({
+    name: 'Luis',
+    course: 'Ingles',
+    age: 30
+  })
+})
+
+
+
+
